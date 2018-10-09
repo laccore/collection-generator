163dbb9678897698af48ebfe02c8ed25dbb8de92
Alexander Stone <enderu@gmail.com>
2018-10-09 11:05:01 -0500

    Switch to importing data from a sqlite database: * import sqlite3 library * use pandas read_sql_query instead of loading from Excel file * update column names * change Date column processing since it's now just imported as text * update instructions/strings to mention database file now.

--- csdco-collection-generator.py
+++ csdco-collection-generator.py
@@ -4,15 +4,15 @@ import simplekml
 import timeit
 import argparse
 import os.path
+import sqlite3
 
 pd.options.mode.chained_assignment = None
 
 def export_csv(dataframe, export_filename):
   export_start_time = timeit.default_timer()
   print('Exporting collection to CSV...')
-  csv_data = dataframe.loc[:,['Location','Country','State_Province','Hole ID','Original ID','Date','Water Depth (m)','Lat','Long','Elevation','Position','Sample Type','mblf T','mblf B','IGSN']]
-  csv_data.loc[:, 'Date'] = csv_data.loc[:, 'Date'].apply(lambda x: x.strftime('%Y%m%d') if isinstance(x, pd.datetime) and pd.notnull(x) else x)
-
+  csv_data = dataframe.loc[:,['Location','Country','State_Province','Hole_ID','Original_ID','Date','Water_Depth','Lat','Long','Elevation','Position','Sample_Type','mblf_T','mblf_B','IGSN']]
+  csv_data.loc[:, 'Date'] = csv_data.loc[:, 'Date'].apply(lambda x: x.replace('-','') if pd.notnull(x) else x)
 
   csv_data.to_csv(export_filename, encoding='utf-8-sig', index=False, float_format='%g')
 
@@ -41,14 +41,14 @@ def export_kml(dataframe, export_filename):
 
   kml_data = dataframe.loc[:,['Location','Long','Lat']]
 
-  kml_data['GECF']= (dataframe['Hole ID'].apply(lambda x: 'LacCoreID: ' + str(x) if pd.notnull(x) else '') + 
-                     dataframe['Original ID'].apply(lambda x: ' / FieldID: ' + str(x) if pd.notnull(x) else '') + 
-                     dataframe['Date'].apply(lambda x: ' / Date: ' + x.strftime('%Y-%m-%d') if pd.notnull(x) else '') + 
-                     dataframe['Water Depth (m)'].apply(lambda x: ' / Water Depth: ' + str(x) + 'm ' if pd.notnull(x) else '') +
-                     dataframe[['mblf T', 'mblf B']].apply(lambda x: (' / Sediment Depth: ' + (str(x[0]) if pd.notnull(x[0]) else '?') + '-' + (str(x[1]) if pd.notnull(x[1]) else '?') + 'm') if (pd.notnull(x[0]) or pd.notnull(x[1])) else '', axis=1) + 
+  kml_data['GECF']= (dataframe['Hole_ID'].apply(lambda x: 'LacCoreID: ' + str(x) if pd.notnull(x) else '') + 
+                     dataframe['Original_ID'].apply(lambda x: ' / FieldID: ' + str(x) if pd.notnull(x) else '') + 
+                     dataframe['Date'].apply(lambda x: ' / Date: ' + str(x) if pd.notnull(x) else '') + 
+                     dataframe['Water_Depth'].apply(lambda x: ' / Water Depth: ' + str(x) + 'm ' if pd.notnull(x) else '') +
+                     dataframe[['mblf_T', 'mblf_B']].apply(lambda x: (' / Sediment Depth: ' + (str(x[0]) if pd.notnull(x[0]) else '?') + '-' + (str(x[1]) if pd.notnull(x[1]) else '?') + 'm') if (pd.notnull(x[0]) or pd.notnull(x[1])) else '', axis=1) + 
                      dataframe['Position'].apply(lambda x: ' / Position: ' + str(x) if pd.notnull(x) else '') + 
                      dataframe['IGSN'].apply(lambda x: ' / IGSN: ' + str(x) if pd.notnull(x) else '') + 
-                     dataframe['Sample Type'].apply(lambda x: ' / Sample Type: ' + str(x) if pd.notnull(x) else ''))
+                     dataframe['Sample_Type'].apply(lambda x: ' / Sample Type: ' + str(x) if pd.notnull(x) else ''))
 
   kml = simplekml.Kml(name='LacCore/CSDCO Core Collection')
   style = simplekml.Style()
@@ -66,31 +66,34 @@ def export_kml(dataframe, export_filename):
   print('Collection exported to {} in {} seconds.\n'.format(export_filename, round(timeit.default_timer()-export_start_time,2)))
 
 
-def process_holes(holes_filename, export_filename):
-  print('Loading {}...'.format(holes_filename))
+def process_holes(holes_database, export_filename):
+  print('Loading {}...'.format(holes_database))
   load_start_time = timeit.default_timer()
-  df = pd.read_excel(holes_filename, parse_dates=False)
-  print('Loaded {} in {} seconds.\n'.format(holes_filename, round(timeit.default_timer()-load_start_time,2)))
+
+  conn = sqlite3.connect(holes_database)
+  df = pd.read_sql_query("SELECT * FROM Holes", conn)
+
+  print('Loaded {} in {} seconds.\n'.format(holes_database, round(timeit.default_timer()-load_start_time,2)))
 
   df.loc[:, ['Lat', 'Long']] = df.loc[:,['Lat', 'Long']].apply(lambda x: round(x, 4))
-  df.loc[:, 'Water Depth (m)'] = df.loc[:,'Water Depth (m)'].apply(lambda x: round(x, 2))
+  df.loc[:, 'Water_Depth'] = df.loc[:,'Water_Depth'].apply(lambda x: round(x, 2))
 
   export_csv(df, export_filename + '.csv')
   export_html(df, export_filename + '.txt')
   export_kml(df, export_filename + '.kml')
 
-  print('Finished processing {} in {} seconds.\n'.format(holes_filename, round(timeit.default_timer()-load_start_time,2)))
+  print('Finished processing {} in {} seconds.\n'.format(holes_database, round(timeit.default_timer()-load_start_time,2)))
 
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Filter and convert the LacCore Holes Excel file into different formats needed for publishing.')
-  parser.add_argument('holes_file', type=str, help='Name of LacCore Holes file.')
+  parser.add_argument('db_file', type=str, help='Name of CSDCO database file.')
   parser.add_argument('-d', action='store_true', help='Export files with the date in the filename (e.g., collection_YYYYMMDD.csv).')
 
   args = parser.parse_args()
 
-  if os.path.isfile(args.holes_file):
+  if os.path.isfile(args.db_file):
     filename = 'collection_' + datetime.datetime.now().strftime('%Y%m%d') if args.d else 'collection'
-    process_holes(args.holes_file, filename)
+    process_holes(args.db_file, filename)
   else:
-    print('ERROR: file \'{}\' does not exist.\n'.format(args.holes_file))
+    print('ERROR: file \'{}\' does not exist.\n'.format(args.db_file))
